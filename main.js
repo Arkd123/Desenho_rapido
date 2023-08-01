454d6456434a60925df117ee9ff3c693e3ebfe8d
Arkd123 <112893425+Arkd123@users.noreply.github.com>
2023-08-01 20:10:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+randomNo = Math.floor((Math.random() *array1.length)+1)
+
+console.log()
+
+var timerCounter = 0;
+var timeCheck = "";
+var drawSketch = "";
+var asnswerHolder = "";
+var score = "";
